c9c5e941c78cf869a305d2d07a60585b6dc6f28b
DanielElias23 <danielchingasilva@gmail.com>
2024-08-03 09:55:35 -0400

    nnada

--- app.py
+++ app.py
@@ -1,34 +1,30 @@
 import streamlit as st
-from transformers import AutoModel, AutoTokenizer   #BertTokenizer, BertForQuestionAnswering
+import numpy as np
+from transformers import BertTokenizer, BertForSequenceClassification
 import torch
 
-tokenizer = AutoModel.from_pretrained("dccuchile/bert-base-spanish-wwm-uncased")
-model = AutoTokenizer.from_pretrained("dccuchile/bert-base-spanish-wwm-uncased")
+@st.cache(allow_output_mutation=True)
+def get_model():
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = BertForSequenceClassification.from_pretrained("pnichite/YTFineTuneBert")
+    return tokenizer,model
 
-st.title("Sistema de preguntas con BETO")
 
-question = st.text_area("Introduce una pregunta")
-#context = st.text_area("Introduce un contexto")
+tokenizer,model = get_model()
 
-if st.button("Responder"):
-            input_ids = tokenizer("Hello, my dog is cuteHello world!", return_tensors="pt")
-            outputs = model(input_ids, labels=input_ids)
-            #loss, prediction_scores = outputs[:2]
-            st.write(f"{outputs}")
-            #st.write(f"{loss} y {prediction_scores}")
+user_input = st.text_area('Enter Text to Analyze')
+button = st.button("Analyze")
 
-       
-"""inputs = tokenizer("Hola")
-        #outputs = model(**inputs)
-        #st.write(outputs) 
-        with torch.no_grad():
-             outputs = model(**inputs)
-             
-        answer_start = torch.argmax(outputs.start_logits)
-        answer_end = torch.argmax(outputs.end_logits) + 1
-        
-        answer_tokens = tokenizer.convert_ids_to_tokens(inputs.input_ids[0][answer_start:answer_end])
-        answer = tokenizer.convert_tokens_to.string(answer_tokens)
-        
-        st.write(f"Respuesta: {answer}")
-"""
+d = {
+    
+  1:'Toxic',
+  0:'Non Toxic'
+}
+
+if user_input and button :
+    test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
+    # test_sample
+    output = model(**test_sample)
+    st.write("Logits: ",output.logits)
+    y_pred = np.argmax(output.logits.detach().numpy(),axis=1)
+    st.write("Prediction: ",d[y_pred[0]])
